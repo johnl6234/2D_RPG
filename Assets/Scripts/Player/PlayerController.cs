cae12a534f62f1108ca5dbe86f52311a57381d67
John Lockham <47853652+johnl6234@users.noreply.github.com>
2023-02-01 21:16:34 +0000

    new anim and attack

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -15,8 +16,11 @@ namespace Scripts.Player
 
         private PlayerInputActions _inputs;
         private InputAction _move;
+        private InputAction _fire;
 
         private Vector2 _movementValue;
+        private bool _attacking = false;
+
         private void Awake()
         {
             _rb = GetComponent<Rigidbody2D>();
@@ -25,22 +29,27 @@ namespace Scripts.Player
             _inputs = new PlayerInputActions();
 
             _move = _inputs.Player.Move;
+            _fire = _inputs.Player.Fire;
 
             _move.performed += Move;
             _move.canceled += Move;
+            _fire.performed += Fire;
+            //_fire.canceled += Fire;
         }
         private void OnEnable()
         {
             _move.Enable();
+            _fire.Enable();
         }
         private void OnDisable()
         {
             _move.Disable();
+            _fire.Disable();
         }
 
         private void FixedUpdate()
         {
-            if (_movementValue != Vector2.zero)
+            if (_movementValue != Vector2.zero && !_attacking)
                 _rb.MovePosition(_rb.position + _movementValue * _moveSpeed * Time.fixedDeltaTime);
 
             _animator.SetFloat("Horizontal", _movementValue.x);
@@ -49,6 +58,17 @@ namespace Scripts.Player
         private void Move(InputAction.CallbackContext value)
         {
             _movementValue = value.ReadValue<Vector2>();
+        }
+
+        private void Fire(InputAction.CallbackContext value)
+        {
+            _attacking = true;
+            _animator.SetTrigger("Attack");
+        }
+
+        private void OnAnimEnd()
+        {
+            _attacking = false;
         }
     }
 
